c8048c808950ed741d73d3da103825b2e55688e2
fabiocuciuffo <cuciuffofab@gmail.com>
2024-10-25 10:54:23 +0200

    my first server

--- api/index.js
+++ api/index.js
@@ -1,1 +1,15 @@
 //api
+import HTTP from "http";
+
+const host = "localhost";
+const port = 8000;
+
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end("My first server!");
+};
+
+const server = HTTP.createServer(requestListener);
+server.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`);
+});
