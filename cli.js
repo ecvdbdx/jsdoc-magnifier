68b69dee242bfaf0c3b78f719b33a7686eb9fef1
Jocabin <kobyfar@hotmail.com>
2024-11-15 11:57:40 +0100

    add cli code

--- cli.js
+++ cli.js
@@ -1,22 +1,24 @@
- 
+#!/usr/bin/env node
+
 import { Command } from 'commander'
 const program = new Command()
 
 program
-  .name('jsdoc-magnifier')
-  .description('CLI JDDoc magnifier')
-  .version('1.0.0')
+        .name('jsdoc-magnifier')
+        .description('CLI JDDoc magnifier')
+        .version('1.0.0')
 
 program
-  .argument('<input>', 'Chemin du fichier d\'entrée')
-  .option('-o, --output <output>', 'Chemin du fichier de sortie')
-  .action((input, options) => {
-    console.log(input)
-    if (options.output) {
-      console.log(options.output)
-    } else {
-      console.log('rien')
-    }
-  });
+        .argument('<input>', 'Chemin du fichier d\'entrée')
+        .option('-o, --output <output>', 'Chemin du fichier de sortie')
+        .action((input, options) => {
+                let outputFilename = input.toString().replace(".js", ".md")
+
+                if (options.output) {
+                        outputFilename = options.output
+                }
+
+
+        });
 
 program.parse()
