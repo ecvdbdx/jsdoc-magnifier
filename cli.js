688961b7fc535dd2b2562f6f458a98809d9a8b27
Jocabin <kobyfar@hotmail.com>
2024-11-15 12:22:39 +0100

    remove useless code

--- cli.js
+++ cli.js
@@ -5,21 +5,21 @@ import { jsDocToMarkdown } from './jsDocToMarkdown.js'
 const program = new Command()
 
 program
-    .name('jsdoc-magnifier')
-    .description('CLI JDDoc magnifier')
-    .version('1.0.0')
+        .name('jsdoc-magnifier')
+        .description('CLI JDDoc magnifier')
+        .version('1.0.0')
 
 program
-    .argument('<input>', "Chemin du fichier d'entrée")
-    .option('-o, --output <output>', 'Chemin du fichier de sortie')
-    .action((input, options) => {
-        let outputFilename = input
+        .argument('<input>', "Chemin du fichier d'entrée")
+        .option('-o, --output <output>', 'Chemin du fichier de sortie')
+        .action((input, options) => {
+                let outputFilename
 
-        if (options.output) {
-            outputFilename = options.output
-        }
+                if (options.output) {
+                        outputFilename = options.output
+                }
 
-        jsDocToMarkdown(input, outputFilename)
-    })
+                jsDocToMarkdown(input, outputFilename)
+        })
 
 program.parse()
